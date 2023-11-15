f1eea39f112b5fdaa41595c558e4c8ed5df103ed
Saraalqhtani8 <62507633+SaraAlqahtani8@users.noreply.github.com>
2023-11-15 07:21:05 +0300

    Initial commit

---
+++ flutter_todoapp/lib/Screens/home.dart
@@ -0,0 +1,94 @@
+//import 'dart:developer';
+//import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_todoapp/constant/color.dart';
+import 'package:flutter_todoapp/Widgets/toDo_item.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar_(),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+        child: Column(
+          children: [
+            Search_Box(),
+            Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 30, bottom: 20),
+                    child: Text(
+                      'All ToDoList',
+                      style: const TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  ToDoItem(),
+                  ToDoItem(),
+                  ToDoItem(),
+                  //sara
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget Search_Box() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.all(0),
+          prefixIcon: Icon(Icons.search, color: Colors.grey, size: 20),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 15,
+            minWidth: 20,
+          ),
+          border: InputBorder.none,
+          hintText: "Search",
+          hintStyle: const TextStyle(color: Colors.grey),
+        ),
+      ),
+    );
+  }
+
+  AppBar AppBar_() {
+    return AppBar(
+        backgroundColor: Colors.grey,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.menu,
+              color: CRed,
+              size: 30,
+            ),
+            Container(
+              height: 40,
+              width: 40,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset('assets/images/Avatar.png'),
+              ),
+            ),
+          ],
+        ));
+  }
+}
---
+++ flutter_todoapp/lib/constant/color.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+const Color BackgraondColor = Color(0xFF212121);
+const Color CRed = Color(0xFFDA4040);
+const Color CBlue = Color(0x000c3550);
+const Color CGray = Color(0xFFE0E0E0);
+
+
+
