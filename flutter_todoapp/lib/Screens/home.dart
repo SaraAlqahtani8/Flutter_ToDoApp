29344cc1930a8821e2c0a5344ef79ad17f5d6614
Saraalqhtani8 <62507633+SaraAlqahtani8@users.noreply.github.com>
2023-11-20 08:24:44 +0300

    TextField and add toDo

--- flutter_todoapp/lib/Screens/home.dart
+++ flutter_todoapp/lib/Screens/home.dart
@@ -6,9 +6,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_todoapp/constant/color.dart';
 import 'package:flutter_todoapp/Widgets/toDo_item.dart';
 import 'package:flutter_todoapp/model/ToDo.dart';
+
 class Home extends StatelessWidget {
-   Home({Key? key}) : super(key: key);
-final todoList=ToDo.todoList();
+  Home({Key? key}) : super(key: key);
+  final todoList = ToDo.todoList();
 
   // This widget is the root of your application.
   @override
@@ -16,33 +17,83 @@ final todoList=ToDo.todoList();
     return Scaffold(
       backgroundColor: Colors.grey,
       appBar: AppBar_(),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-        child: Column(
-          children: [
-            Search_Box(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(top: 30, bottom: 20),
-                    child: Text(
-                      'All ToDoList',
-                      style: const TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w500,
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Column(
+              children: [
+                Search_Box(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(top: 30, bottom: 20),
+                        child: Text(
+                          'All ToDoList',
+                          style: const TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
                       ),
+                      for (ToDo toDo in todoList)
+                        ToDoItem(
+                          toDo: toDo,
+                        ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(bottom: 20, right: 20, left: 20),
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Colors.black12,
+                      boxShadow: const [
+                        BoxShadow(
+                          color: CBlue,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 10.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Add new toDo item!',
+                          border: InputBorder.none),
                     ),
                   ),
-                  for(ToDo toDo in todoList)
-                  ToDoItem(toDo: toDo,),
-                
-                 
-                ],
-              ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(bottom: 20, right: 20),
+                  child: ElevatedButton(
+                    child: Text(
+                      '+',
+                      style: TextStyle(
+                        fontSize: 40,
+                      ),
+                    ),
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        primary: CRed,
+                        minimumSize: Size(60, 60),
+                        elevation: 10),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
